b1835fd19f9b73aef0e7e6b14c984cd13183a8ed
BeStardust <wu.zt@foxmail.com>
2022-03-21 18:15:05 +0800

    PID

--- PID/src/PID.java
+++ PID/src/PID.java
@@ -6,15 +6,15 @@ public class PID {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
-        System.out.println(runtimeMXBean.getName());
-        while(true) {
-            try {
+		System.out.println(runtimeMXBean.getName());
+		while (true) {
+			try {
 				Thread.sleep(0);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
+		}
 	}
 
 }
